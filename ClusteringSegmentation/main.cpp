1ec7f41e6b1f9b9ef3db0df31ca3f62312cd2806
Mo DeJong <mo@modejong.com>
2015-12-30 13:01:45 -0800

    Initial Commit

---
+++ ClusteringSegmentation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ClusteringSegmentation
+//
+//  Created by Mo DeJong on 12/30/15.
+//  Copyright © 2015 helpurock. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
